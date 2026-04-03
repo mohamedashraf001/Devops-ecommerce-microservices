43dc866a1fbddc8500c0c263c2243f5fc247e25a
mohamedashraf001 <mohamedashraf11ma11@gmial.com>
2026-04-03 16:57:32 +0200

    fix jenkins shared library run stage

--- jenkins-shared-library/vars/dockerRun.groovy
+++ jenkins-shared-library/vars/dockerRun.groovy
@@ -1,11 +1,14 @@
 def call(String containerName, String imageName, String port) {
     sh """
-    echo "Running container: ${containerName}"
+    echo "Stopping old container if exists..."
 
     docker stop ${containerName} || true
     docker rm ${containerName} || true
 
-    docker run -d --name ${containerName} \
+    echo "Running new container..."
+
+    docker run -d \
+    --name ${containerName} \
     -p ${port}:${port} \
     ${imageName}:latest
     """
